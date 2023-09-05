c8a8a8e522e20d3f7f6cdea1588930bd1df6a430
AlejoVeraCode <114888351+AlejoVeraCode@users.noreply.github.com>
2023-09-05 15:27:39 -0300

    Update About.jsx

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -7,20 +7,38 @@ const About = () => {
             <div className='About'>
                 <div className='AboutContainer'>
                     <h1 className='AboutTitle'>Hola! Soy Alejo <br/>
-                    Desarrollador de Software Junior con Experiencia en Electromecánica: Construyendo soluciones digitales con conocimientos técnicos sólidos</h1>
+                    Desarrollador de Software Junior Construyendo soluciones digitales con conocimientos técnicos sólidos</h1>
                     
                         <div className="ImageContainer">
                     <img src="https://i.postimg.cc/Xvf9m3n8/Alejo.jpg" alt="Alejo FullStack" />
                      </div>
-                    <p className='AboutTitleP'> Como profesional con una destacada trayectoria en el campo del mantenimiento electromecanico, y la refrigeracion industrial, poseo habilidades técnicas sólidas y habilidades blandas que son altamente transferibles al emocionante mundo del desarrollo de software. <br/>
-                     Mi experiencia diversa y mis logros demuestran mi capacidad para enfrentar desafíos técnicos con éxito y tomar decisiones efectivas en entornos dinámicos.<br/>
-                     En mi primera experiencia en relacion de dependencia en JBS Swift S.A gracias a mi habilidad para el analisis de fallas, la resolucion de problemas y mi predisposicion para el trabajo en equipo, en tan solo 3 meses pase de ser un tecnico de mantenimiento junior a estar encargado del turno donde era responsable de una instalacion frigorifica de alta complejidad con mas de 24.000 litros de amoniaco en estado liquido y valuada en mas de 26 millones de USD.
-                     <br/>
-                     Mi experiencia como electricista freelance me permitió desarrollar habilidades en instalaciones residenciales, comerciales e industriales. Lideré proyectos que implicaron el armado de cañerías, cableado y lectura de planos, así como la instalación de motores monofásicos y trifásicos. Mi habilidad para trabajar en diversos entornos, realizar tareas de herrería, albañilería y pintura, y ejecutar proyectos de automatización con contactores, demuestra mi versatilidad y capacidad para afrontar desafíos complejos.<br/>
-                     En mi rol como técnico de mantenimiento en un centro de distribución de Carrefour, desarrollé competencias en mantenimiento electromecánico y en la operación de sistemas de refrigeración de alta complejidad, teniendo a cargo una instalacion con mas de 18.000 litros de amoniaco. Demostré habilidades destacadas en la resolución de problemas relacionados con compresores, PLC, variadores y grupos electrógenos. Además, gestioné exitosamente proyectos de mantenimiento edilicio, climatización y electricidad en general. Estas habilidades me han permitido comprender y abordar sistemas técnicos sofisticados, lo cual resulta invaluable en el desarrollo de software confiable y eficiente.<br/>
-                    En mi rol anterior como técnico de servicio en Mayekawa Argentina S.A, adquirí conocimientos en la reparación y puesta en marcha de equipos de refrigeración en varias provincias de Argentina, Uruguay y Paraguay, tambien fui seleccionado dentro del equipo para realizar un curso de PLC en Sao Pablo, Brasil. Demostré habilidades en el manejo de instrumentos de precisión, alineación de motores y el uso de arrancadores suaves, variadores y PLC. Esta experiencia me ha enseñado a adaptarme rápidamente a nuevas tecnologías y a trabajar eficientemente en equipos multidisciplinarios.<br/>
-                    En la actualidad como responsable de mantenimiento en una reconocida clínica de neurorehabilitación, adquirí habilidades especializadas en el trato con clientes internos, proveedores, gestion de tiempos y recursos. Mi enfoque meticuloso y mi atención al detalle garantizan la resolución eficiente de problemas complejos, características esenciales para asegurar la calidad en el desarrollo de software.<br/>
-En resumen, mi amplia experiencia en el campo del mantenimiento, la electricidad y la refrigeracion industrial respalda mis habilidades técnicas sólidas, tales como la resolución de problemas, la atención al detalle y la adaptabilidad tecnológica. Mis habilidades blandas, como la toma de decisiones efectivas, el trabajo en equipo y el aprendizaje rápido, fortalecen mi capacidad para integrarme rápidamente en su equipo de desarrollo de software. Estoy emocionado por aplicar mi experiencia y conocimientos en el desarrollo de software para impulsar el éxito de su empresa.</p>
+                    <p className='AboutTitleP'> Soy Alejo Vera, Desarrollador de Software Junior
+
+Tengo experiencia en el campo del desarrollo de software. Mi conjunto de habilidades incluye HTML, CSS, JavaScript y React. A continuación, destacaré mis capacidades y enfoque profesional.
+
+Habilidades Clave:
+
+HTML y CSS: Creo sitios web funcionales y estéticos utilizando HTML y CSS para brindar una experiencia de usuario agradable.
+
+JavaScript: Tengo experiencia en el desarrollo de interacciones web utilizando JavaScript para crear funcionalidades dinámicas.
+
+React: Trabajo con React para construir aplicaciones web modernas y eficientes, centrándome en la creación de componentes reutilizables.
+
+Tecnico Electromecánico: Además de la programación, tengo experiencia en mantenimiento electromecánico, lo que me brinda una perspectiva técnica y resolutiva en mi trabajo.
+
+Enfoque Profesional:
+
+Mi enfoque es encontrar soluciones efectivas a los desafíos que se me presentan. Estoy comprometido con el aprendizaje constante y busco contribuir al éxito de los proyectos en los que participo.
+
+Visión:
+
+Como desarrollador junior, mi objetivo es seguir mejorando y creciendo en mi carrera. Aspiro a convertirme en un profesional altamente calificado en el desarrollo de software y aportar valor a proyectos futuros.
+
+¡Hablemos!
+
+Si estás interesado en colaborar o tienes alguna pregunta, no dudes en contactarme. Estoy abierto a oportunidades y desafíos en el desarrollo de software y el mantenimiento electromecánico.
+
+Gracias por visitar mi página y conocer más sobre mi experiencia y habilidades. Espero tener la oportunidad de trabajar contigo en proyectos futuros.</p>
                 </div>
             </div>
         </section>
