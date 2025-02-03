1e421a6ea2067c3a0b003ee325fcf072acaa646c
AlejoVeraCode <114888351+AlejoVeraCode@users.noreply.github.com>
2025-02-03 11:01:37 -0300

    Update Footer.jsx

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -4,7 +4,7 @@ const Footer = () => {
     return (
         <div className='FooterContainer'>
     <footer>
-      <p className="Copy">&copy; Copyright 2023</p>
+      <p className="Copy">&copy; Copyright 2024</p>
       <p className='Copy'>
         Desarrollado por 
         <a href="https://github.com/AlejoVeraCode" target="_blank">
